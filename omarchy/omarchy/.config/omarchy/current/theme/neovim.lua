3c0d6e02b91c74ff24cbbb02629059e0bf4efdb9
trankhanh040147 <trankhanh040147@gmail.com>
2026-03-10 07:37:10 +0700

    omarchy: sync

--- omarchy/omarchy/.config/omarchy/current/theme/neovim.lua
+++ omarchy/omarchy/.config/omarchy/current/theme/neovim.lua
@@ -1,13 +1,9 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-	},
-	{
+  { "tahayvr/matteblack.nvim", lazy = false, priority = 1000 },
+  {
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "matteblack",
 		},
 	},
 }
